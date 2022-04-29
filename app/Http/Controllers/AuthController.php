3601f0261788a9c2025ae578bae5a100d924ae4d
cchura94 <cchura.cpc@gmail.com>
2022-04-28 21:46:04 -0400

    Proyecto inicial + Login Sanctum

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // login
+    public function login(Request $request)
+    {
+        // validar
+        $request->validate([
+            "email" => "required|email|string",
+            "password" => "required|string"
+        ]);
+        // verificar las credenciales
+        $credentials = request(["email", "password"]);
+        if(!Auth::attempt($credentials)){
+            return response()->json([
+                "mensaje" => "No Autorizado",
+                "error" => true
+            ], 401);
+        }
+        // generar token
+        $user = $request->user();
+        $tokenResult = $user->createToken("mi_token_login");
+        $token = $tokenResult->plainTextToken;
+        // reponder token
+        return response()->json([
+            "access_token" => $token,
+            "token_type" => "Bearer",
+            "error" => false
+        ], 200);
+    }
+
+    // registro
+    public function registro(Request $request)
+    {
+        // validar
+        $request->validate([
+            "name" => "required|string",
+            "email" => "required|email|string|unique:users",
+            "password" => "required|string",
+            "c_password" => "required|same:password"
+        ]);
+        // guardar
+        $usuario = new User;
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = Hash::make($request->password);
+        $usuario->save();
+        // opcional generar token
+        $tokenResult = $usuario->createToken("mi_token");
+        $token = $tokenResult->plainTextToken;
+
+        // responder        
+        return response()->json([
+            "mensaje" => "Registro correcto",
+            "acces_token" => $token
+        ], 201);
+    }
+
+    // perfil
+    public function perfil(Request $request)
+    {
+        // return $request->user();
+        // return Auth::user();
+        $user = auth()->user();
+        return response()->json($user, 200);        
+    }
+
+    // logout
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            "mensaje" => "Logout"
+        ]);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+// AUTH SANCTUM
+Route::group(["prefix" => "/v1/auth"], function(){
+    Route::post("/login", [AuthController::class, "login"]);
+    Route::post("/registro", [AuthController::class, "registro"]);
+
+    Route::group(["middleware" => "auth:sanctum"], function(){
+        Route::get("/perfil", [AuthController::class, "perfil"]);
+        Route::post("/logout", [AuthController::class, "logout"]);
+    });
+});
+
+
